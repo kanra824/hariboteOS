3b3c4bc1c6ec78cdbde5522d98cc24046e389800
Md <mdyh2a380824@gmail.com>
2019-05-03 23:18:26 +0900

    harib01f

--- tolset/src/bootpack.c
+++ tolset/src/bootpack.c
@@ -1,16 +1,66 @@
 void io_hlt(void);
-void write_mem8(int addr, int data);
+void io_cli(void);
+void io_out8(int port, int data);
+int  io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
 
 void HariMain(void)
 {
 	int i;
+	char *p;
 
-	for(i = 0xa0000; i <= 0xaffff; i++) {
-		write_mem8(i, i & 0x0f); // MOV BYTE [i], 15
+	init_palette();
+
+	p = (char *) 0xa0000;
+
+	for(i = 0; i <= 0xffff; i++) {
+		p[i] = i & 0x0f;
 	}
 
-	for(;;) {
+	for (;;) {
 		io_hlt();
 	}
+}
 
+void init_palette(void)
+{
+	static unsigned char table_rgb[16 * 3] = {
+		0x00, 0x00, 0x00, // 0: black 
+		0xff, 0x00, 0x00, // 1: bright red 
+		0x00, 0xff, 0x00, // 2: bright green 
+		0xff, 0xff, 0x00, // 3: bright yellow 
+		0x00, 0x00, 0xff, // 4: bright blue
+		0xff, 0x00, 0xff, // 5: bright purple
+		0x00, 0xff, 0xff, // 6: bright light blue
+		0xff, 0xff, 0xff, // 7: white
+		0xc6, 0xc6, 0xc6, // 8: bright gray
+		0x84, 0x00, 0x00, // 9: dark red
+		0x00, 0x84, 0x00, // 10: dark green
+		0x84, 0x84, 0x00, // 11: dark yellow
+		0x00, 0x00, 0x84, // 12: dark blue
+		0x84, 0x00, 0x84, // 13: dark purple
+		0x00, 0x84, 0x84, // 14: dark light blue
+		0x84, 0x84, 0x84  // 15: dark gray
+	};
+	set_palette(0, 15, table_rgb);
+	return ;
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+	int i, eflags;
+	eflags = io_load_eflags();
+	io_cli();
+	io_out8(0x03c8, start);
+	for(i = start; i <= end; i++) {
+		io_out8(0x03c9, rgb[0] / 4);
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
+		rgb += 3;
+	}
+	io_store_eflags(eflags);
+	return;
 }
