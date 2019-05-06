5fd08e7ba56c1cca53c9ffdb95c479499cfb59d5
Md <mdyh2a380824@gmail.com>
2019-05-07 00:19:58 +0900

    Add FIFO buffer

--- tolset/bootpack.c
+++ tolset/bootpack.c
@@ -3,19 +3,18 @@
 #include "bootpack.h"
 #include <stdio.h>
 
-extern struct FIFO8 keyfifo;
+extern struct KEYBUF keybuf;
 
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
-	char s[40], mcursor[256], keybuf[32];
+	char s[40], mcursor[256];
 	int mx, my, i;
 
 	init_gdtidt();
 	init_pic();
 	io_sti(); // IDT/PICの初期化が終わったのでCPUの割り込み禁止を解除
 
-	fifo8_init(&keyfifo, 32, keybuf);
 	io_out8(PIC0_IMR, 0xf9); // PIC1とキーボードを許可(11111001)
 	io_out8(PIC1_IMR, 0xef); // マウスを許可(11101111)
 
@@ -30,10 +29,15 @@ void HariMain(void)
 
 	for (;;) {
 		io_cli();
-		if(fifo8_status(&keyfifo) == 0) {
+		if (keybuf.len == 0) {
 			io_stihlt();
 		} else {
-			i = fifo8_get(&keyfifo);
+			i = keybuf.data[keybuf.next_r];
+			keybuf.len--;
+			keybuf.next_r++;
+			if (keybuf.next_r == 32) {
+				keybuf.next_r = 0;
+			}
 			io_sti();
 			sprintf(s, "%02X", i);
 			boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
--- tolset/src/int.c
+++ tolset/src/int.c
@@ -1,4 +1,5 @@
 #include "bootpack.h"
+#include <stdio.h>
 
 void init_pic(void)
 {
@@ -12,7 +13,7 @@ void init_pic(void)
     
     io_out8(PIC1_ICW1, 0x11); // エッジトリガモード
     io_out8(PIC1_ICW2, 0x28); // IRQ8-15はINT28-2fで受ける
-    io_out8(PIC1_ICW3, 2); // PIC1はIRQ2にて接続
+    io_out8(PIC1_ICW3, 2);    // PIC1はIRQ2にて接続
     io_out8(PIC1_ICW4, 0x01); // ノンバッファモード
     
     io_out8(PIC0_IMR, 0xfb); // 11111011 PIC1以外すべて禁止
@@ -21,14 +22,17 @@ void init_pic(void)
     return;
 }
 
+#define PORT_KEYDAT     0x0060
+
+struct FIFO8 keyfifo;
+
 void inthandler21(int *esp)
 {
-    struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
-    boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 0, 32 * 8 - 1, 15);
-    putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, "INT 21 (IRQ-1) : PS/2 keyboard");
-    for(;;) {
-        io_hlt();
-    }
+    unsigned char data;
+    io_out8(PIC0_OCW2, 0x61);
+    data = io_in8(PORT_KEYDAT);
+    fifo8_put(&keyfifo, data);
+    return;
 }
 
 void inthandler2c(int *esp)
